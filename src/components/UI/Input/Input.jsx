ee35c34556acb3e0f93d679c325202c7c62e60c9
szymek325 <matszym325@gmail.com>
2020-03-16 09:01:13 +0100

    Add Input module

---
+++ src/components/UI/Input/Input.jsx
@@ -0,0 +1,25 @@
+﻿import React from 'react';
+import classes from './Input.module.css'
+
+const input = (props) => {
+    let inputElement = null;
+    switch (props.inputtype) {
+        case('input'):
+            inputElement = <input className={classes.InputElement} type="text" {...props}/>;
+            break;
+        case('textarea'):
+            inputElement = <textarea name="" id="" cols="30" rows="10"/>;
+            break;
+        default:
+            inputElement = <input/>
+    }
+
+    return (
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.label}</label>
+            {inputElement}
+        </div>
+    );
+};
+
+export default input;
--- src/containers/ContactData/ContactData.jsx
+++ src/containers/ContactData/ContactData.jsx
@@ -3,6 +3,7 @@ import Button from "../../components/UI/Button/Button";
 import classes from './ContactData.module.css'
 import axios from "../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import Input from "../../components/UI/Input/Input";
 
 class ContactData extends Component {
     state = {
@@ -41,10 +42,10 @@ class ContactData extends Component {
 
     render() {
         let form = (<form>
-            <input className={classes.Input} type="text" name='name' placeholder='Name'/>
-            <input className={classes.Input} type="text" name='email' placeholder='Email'/>
-            <input className={classes.Input} type="text" name='street' placeholder='Street Name'/>
-            <input className={classes.Input} type="text" name='postal' placeholder='Postal Code'/>
+            <Input inputtype="input" type="text" name='name' placeholder='Name'/>
+            <Input inputtype="input" type="text" name='email' placeholder='Email'/>
+            <Input inputtype="input" type="text" name='street' placeholder='Street Name'/>
+            <Input inputtype="input" type="text" name='postal' placeholder='Postal Code'/>
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>);
         if (this.state.loading) {
