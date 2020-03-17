936ca3b9f9a90b691f0bf60c87b38000d9d98ca8
szymek325 <matszym325@gmail.com>
2020-03-17 21:44:05 +0100

    Add generic inputs

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -3,15 +3,39 @@ import classes from './Input.module.css'
 
 const input = (props) => {
     let inputElement = null;
-    switch (props.inputtype) {
+    switch (props.elementType) {
         case('input'):
-            inputElement = <input className={classes.InputElement} type="text" {...props}/>;
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed}/>;
             break;
         case('textarea'):
-            inputElement = <textarea name="" id="" cols="30" rows="10"/>;
+            inputElement = <textarea
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed}/>;
+            break;
+        case('select'):
+            inputElement = <select
+                className={classes.InputElement}
+                value={props.value}
+                onChange={props.changed}>
+                {props.elementConfig.options.map(option => (
+                    <option key={option.value} value={option.value}>
+                        {option.displayValue}
+                    </option>
+                ))}
+            </select>;
             break;
         default:
-            inputElement = <input/>
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed}/>;
     }
 
     return (
